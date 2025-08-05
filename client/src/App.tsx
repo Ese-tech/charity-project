778a510733bbfd499665e213bf6bdb199bad7067
Ese-tech <ese.osagie19@gmail.com>
2025-08-05 23:44:20 +0200

    refactor: clean up commented code in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,14 @@
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  // const [] = useState(0)
 
   return (
     <>
-      
+    
     </>
   )
 }
 
 export default App
+
